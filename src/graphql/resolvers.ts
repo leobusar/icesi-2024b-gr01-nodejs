85d4955f0e7dcc172afe8ebfca8bfa8e10388df8
leobusta <leobusta@gmail.com>
2024-10-30 10:06:41 -0500

    create user graphql

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -1,4 +1,4 @@
-import { UserDocument } from "../models/user.model";
+import { UserDocument, UserInput } from "../models/user.model";
 import userService from "../services/user.service";
 
 export const resolvers = {
@@ -7,8 +7,21 @@ export const resolvers = {
             console.log(params.id);
             const user: UserDocument | null = await userService.findById(params.id);
             return user;
+        }, 
+        users: (_root: any) => userService.findAll(),
+        createUser: async (_root: any, params: any )=> {
+            const userOutput: UserDocument = await userService.create(params.input as UserInput);
+            return userOutput;
         }
 
     },
+    Mutation: {
+        createUser: async (_root: any, params: any )=> {
+            const userOutput: UserDocument = await userService.create(params.input as UserInput);
+            return userOutput;
+        }
+    }
+
+
 
 }
