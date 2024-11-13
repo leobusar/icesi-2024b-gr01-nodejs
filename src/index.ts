7827df0ad71bf35d90bdf80ac4555bb1a3720964
Leonardo Bustamante <leobusta@gmail.com>
2024-11-13 14:19:47 -0500

    added cors

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 import { router } from './routes/posts';
 import { router as user } from './routes/user';
@@ -9,6 +10,7 @@ dotenv.config();
 
 const app: Express = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
